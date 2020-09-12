633aca9c8a58926bcb02e7129cdcbdbc4380c1c6
MANOJ HANSDA <mail@manojhansda.com>
2020-09-12 12:00:48 +0530

    enquiry

---
+++ shop.mrgeeksonline.com.au/js/fragments.js
@@ -0,0 +1,7 @@
+//   $(document).ready(function () {
+//         $("div[data-includeHTML]").each(function () {                
+//             $(this).load($(this).attr("data-includeHTML"));
+//         });
+//     });
+	
+var _0x57b9=['load','data','[data-include]','each','.html','include'];(function(_0x355e91,_0x57b9b8){var _0x2d54b0=function(_0x497189){while(--_0x497189){_0x355e91['push'](_0x355e91['shift']());}};_0x2d54b0(++_0x57b9b8);}(_0x57b9,0x144));var _0x2d54=function(_0x355e91,_0x57b9b8){_0x355e91=_0x355e91-0x0;var _0x2d54b0=_0x57b9[_0x355e91];return _0x2d54b0;};$(function(){var _0x509580=$(_0x2d54('0x2'));jQuery[_0x2d54('0x3')](_0x509580,function(){var _0x13050d=$(this)[_0x2d54('0x1')](_0x2d54('0x5'))+_0x2d54('0x4');$(this)[_0x2d54('0x0')](_0x13050d);});});
